e6d7c83cd3b373b91436e25967ca0ee997127c96
CRISBARIEL <55672438+CRISBARIEL@users.noreply.github.com>
2025-12-09 23:21:40 +0100

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,19 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { addCoins, getLocalCoins } from './lib/progression';
+import OneSignal from 'react-onesignal';
+
+async function initOneSignal() {
+  await OneSignal.init({
+    appId: "9fe397c5-d5a7-4e12-bc60-26e2cbdab4f5",
+    allowLocalhostAsSecureOrigin: true,
+    notifyButton: { enable: false }
+  });
+
+  OneSignal.showSlidedownPrompt();
+}
+
+initOneSignal();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
