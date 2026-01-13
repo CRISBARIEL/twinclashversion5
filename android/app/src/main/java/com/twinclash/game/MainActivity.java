23e0d2baa0a1f6fc777cf6c496f6a2c8570d188c
CRISBARIEL <55672438+CRISBARIEL@users.noreply.github.com>
2026-01-13 09:15:18 +0100

    Updated settings.gradle

--- android/app/src/main/java/com/twinclash/game/MainActivity.java
+++ android/app/src/main/java/com/twinclash/game/MainActivity.java
@@ -2,10 +2,17 @@ package com.twinclash.game;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends BridgeActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    new Thread(
+      () -> {
+        MobileAds.initialize(this, initializationStatus -> {});
+      }
+    ).start();
   }
 }
